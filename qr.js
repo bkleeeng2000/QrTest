3f301aec2840cd6cc4fdc999822758d22ac99f95
bkleeeng2000 <bkleeeng2000@naver.com>
2021-04-27 17:47:28 +0900

    Add files via upload

--- qr.js
+++ qr.js
@@ -1,6 +1,9 @@
 function onScanSuccess(qrCodeMessage) {
     console.log(qrCodeMessage);
+    const text = CryptoJS.AES.decrypt(qrCodeMessage,"00000000000000000000005x8w5z6w8q")
+    console.log(text);
 }
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 var resultContainer = document.getElementById('reaader-result');
 html5QrcodeScanner.render(onScanSuccess);
+
