0d2e8c3fb4d897f7e3dc8db26ee0272b0470b4be
bkleeeng2000 <bkleeeng2000@naver.com>
2021-04-29 23:47:24 +0900

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,7 +1,22 @@
 function onScanSuccess(qrCodeMessage) {
+    html5QrcodeScanner.clear();
     resultContainer.innerText = qrCodeMessage;
+    fetch("/crypto", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({text: qrCodeMessage})
+    }).then((response) => {
+        response.text()
+            .then((text) => {
+                console.log(text)
+                decryptContainer.innerText = qrCodeMessage;
+            })
+    });
 }
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 var resultContainer = document.getElementById('reader-result');
+var decryptContainer = document.getElementById('decrypt-result');
 html5QrcodeScanner.render(onScanSuccess);
 
